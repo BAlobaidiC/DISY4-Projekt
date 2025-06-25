c76bed05fdc226799128272affcab3ac5ecee554
tarekh-97 <tarek_hitalani@yahoo.com>
2025-06-26 00:13:00 +0200

    Comments UsageService

--- DISY-4/DISYS_Gruppe4/Backend/energy-community-usage_service/src/main/java/fhtw/at/energy_community_usage_service/entity/UsageHour.java
+++ DISY-4/DISYS_Gruppe4/Backend/energy-community-usage_service/src/main/java/fhtw/at/energy_community_usage_service/entity/UsageHour.java
@@ -12,12 +12,16 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @Entity
+// Repräsentiert die Energienutzung einer Stunde
 public class UsageHour {
-
+    // Zeitpunkt der Stunde (Primärschlüssel)
     @Id
     private LocalDateTime hour;
 
+    // Von der Gemeinschaft produzierte Energie
     private double communityProduced;
+    // Von der Gemeinschaft verbrauchte Energie
     private double communityUsed;
+    // Aus dem Netz genutzte Energie
     private double gridUsed;
 }
