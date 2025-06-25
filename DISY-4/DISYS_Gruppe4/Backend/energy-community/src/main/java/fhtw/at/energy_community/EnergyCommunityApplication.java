1d7b424b13829c8e10d8488c9916da5642e3deff
BAlobaidiC <bilalalobaidi.bl@gmail.com>
2025-06-25 22:37:33 +0200

    comment EnergyMessage

--- DISY-4/DISYS_Gruppe4/Backend/energy-community/src/main/java/fhtw/at/energy_community/EnergyCommunityApplication.java
+++ DISY-4/DISYS_Gruppe4/Backend/energy-community/src/main/java/fhtw/at/energy_community/EnergyCommunityApplication.java
@@ -5,14 +5,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+/**
+ * Hauptanwendungsklasse für die Energy Community Anwendung.
+ * 
+ * @EnableScheduling - Aktiviert die Zeitplanung für geplante Aufgaben
+ * @EnableRabbit - Aktiviert die RabbitMQ-Integration für Messaging
+ */
 @SpringBootApplication
 @EnableScheduling
 @EnableRabbit
-
 public class EnergyCommunityApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EnergyCommunityApplication.class, args);
-	}
-
+    /**
+     * Hauptmethode zum Starten der Spring Boot Anwendung
+     * 
+     * @param args Kommandozeilenargumente
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(EnergyCommunityApplication.class, args);
+    }
 }
--- DISY-4/DISYS_Gruppe4/Backend/energy-community/src/main/java/fhtw/at/energy_community/model/EnergyMessage.java
+++ DISY-4/DISYS_Gruppe4/Backend/energy-community/src/main/java/fhtw/at/energy_community/model/EnergyMessage.java
@@ -4,12 +4,22 @@ import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * Repräsentiert eine Energienachricht im System.
+ * Diese Klasse dient zur Speicherung von Energieverbrauchsdaten.
+ */
 @Getter
 @Setter
 public class EnergyMessage {
+    /** Der Typ der Energienachricht */
     private String type;
+    
+    /** Die zugehörige Verbindung oder Gemeinschaft */
     private String association;
+    
+    /** Der Energieverbrauch in Kilowattstunden */
     private double kwh;
+    
+    /** Zeitstempel der Nachricht */
     private LocalDateTime datetime;
-
 }
